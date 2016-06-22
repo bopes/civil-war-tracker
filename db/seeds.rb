ffaa64c7f717fcdd84a00d4684e9924b0c9d9591
bopes <clcblee@gmail.com>
2016-06-22 16:14:15 -0700

    Add army seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
-Side.create!(name: "The Union")
-Side.create!(name: "The Confederacy")
+union = Side.create!(name: "The Union")
+confederacy = Side.create!(name: "The Confederacy")
+
+army_potomac = Army.create!(name: "Army of the Potomac", side: union)
+army_northern_va = Army.create!(name: "Army of Northern Virginia", side: confederacy)
