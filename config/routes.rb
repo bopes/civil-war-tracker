f893f4c045d9ccc1b0a7fdd8b2d219f12abca47a
bopes <clcblee@gmail.com>
2016-06-22 18:25:09 -0700

    Add initial member resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  resources :sides, only: [:index, :show]
-  resources :armies, only: [:index, :show]
+  resources :sides, only: [:index, :show] do
+    member do
+      resources :players, only: [:index, :show]
+      resources :armies, only: [:index, :show] do
+        member do
+          resources :campaigns, only: [:index, :show]
+        end
+      end
+    end
+  end
 
 end
