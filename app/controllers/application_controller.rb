0ba4842df37ade56db16b90ff258f6e8ba24220a
bopes <clcblee@gmail.com>
2016-06-23 15:15:16 -0700

    Add find_union, find_confederacy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def find_union(search_class)
+    union = Side.find_by(name: "The Union")
+    search_class.where(side: union)
+  end
+
+  def find_confederates(search_class)
+    confederacy = Side.find_by(name: "The Confederacy")
+    search_class.where(side: confederacy)
+  end
+
 end
