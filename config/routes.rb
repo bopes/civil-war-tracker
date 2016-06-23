ebd684c9af45d88217dd2eecf91e7be34ce867e8
bopes <clcblee@gmail.com>
2016-06-22 22:45:44 -0700

    Remove nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,9 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  resources :sides, only: [:index, :show] do
-    member do
-      resources :players, only: [:index, :show]
-      resources :armies, only: [:index, :show] do
-        member do
-          resources :campaigns, only: [:index, :show]
-        end
-      end
-    end
-  end
+  resources :sides, only: [:index, :show]
+  resources :players, only: [:index, :show]
+  resources :armies, only: [:index, :show]
+  resources :campaigns, only: [:index, :show]
 
 end
