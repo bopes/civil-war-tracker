baeda57ffe12032ff7c66c8298d9dc67e308ec29
bopes <clcblee@gmail.com>
2016-06-23 15:19:19 -0700

    Finish index method

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,7 +1,13 @@
 class CampaignsController < ApplicationController
   def index
+    union = Side.find_by(name: "The Union")
+    @union_campaigns = Campaign.all.select { |campaign| campaign.side == union }
+
+    confederacy = Side.find_by(name: "The Confederacy")
+    @confederate_campaigns = Campaign.all.select { |campaign| campaign.side == confederacy }
   end
 
   def show
+    @campaign = Campaign.find(params[:id])
   end
 end
