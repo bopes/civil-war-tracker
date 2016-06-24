acecb4827c17e420d60186a4a5cca135edc60e70
bopes <clcblee@gmail.com>
2016-06-23 19:53:34 -0700

    Add events and battles

--- db/migrate/20160623225820_create_events.rb
+++ db/migrate/20160623225820_create_events.rb
@@ -1,10 +1,11 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-      t.text :description, null: false
-      t.date :date, null: false
-      t.integer :type
-      t.integer :battle_id
+      t.text :desc, null: false
+      t.date :begin_date, null: false
+      t.date :end_date
+      t.references :link, polymorphic: true, index: true
+      t.integer :location_id
 
       t.timestamps null: false
     end
--- db/migrate/20160623230610_create_battles.rb
+++ db/migrate/20160623230610_create_battles.rb
@@ -1,11 +1,9 @@
 class CreateBattles < ActiveRecord::Migration
   def change
     create_table :battles do |t|
-      t.integer :event_id
-      t.date :begin_date
-      t.date :end_date
-      t.integer :campaign_id
-      t.integer :victor_id
+      t.references :event
+      t.references :campaign
+      t.references :victor
       t.string :conclusion
 
       t.timestamps null: false
