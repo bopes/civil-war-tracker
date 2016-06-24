c1428307c4ef5b36ff7f1f2e1900d2d64d0f49af
bopes <clcblee@gmail.com>
2016-06-23 20:30:32 -0700

    Add battle routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'battles/index'
-
-  get 'battles/show'
-
   root 'static_pages#home'
 
   resources :sides, only: [:index, :show]
   resources :players, only: [:index, :show]
   resources :armies, only: [:index, :show]
   resources :campaigns, only: [:index, :show]
+  resources :battles, only: [:index, :show]
 
 end
