a3df7f545fb0ffb8811e9d15ecf60a42223193ab
bopes <clcblee@gmail.com>
2016-06-23 19:51:47 -0700

    Add associations

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,2 +1,9 @@
 class Battle < ActiveRecord::Base
+
+  belongs_to :campaign, inverse_of: :battles
+  has_one :event, as: :link, inverse_of: :link
+  has_and_belongs_to_many :players
+  has_and_belongs_to_many :armies
+  belongs_to :victor, class_name: "Side"
+
 end
