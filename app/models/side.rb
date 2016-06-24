54cf46a66eb90d8352f85fa370b49f887fd31841
bopes <clcblee@gmail.com>
2016-06-23 19:53:07 -0700

    Add victory association

--- app/models/side.rb
+++ app/models/side.rb
@@ -1,7 +1,8 @@
 class Side < ActiveRecord::Base
 
-  has_many :armies
+  has_many :armies, inverse_of: :side
   has_many :campaigns, through: :armies
-  has_many :players
+  has_many :players, inverse_of: :side
+  has_many :victories, class_name: "Battle", foreign_key: "victor_id"
 
 end
