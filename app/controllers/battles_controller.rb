b0ee479c0ae74cb018bfc2782de42b4402258771
bopes <clcblee@gmail.com>
2016-06-23 20:30:06 -0700

    Add index method

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -1,5 +1,12 @@
 class BattlesController < ApplicationController
   def index
+    @battles = Array.new
+    Event.where(link_type: "Battle").each do |event|
+      @battles << { battle: event.link,
+                    date: event.begin_date,
+                    name: event.desc,
+                    victor: event.link.victor.name }
+    end
   end
 
   def show
