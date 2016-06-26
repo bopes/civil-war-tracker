47c332b819f7bf8cbd45132e6d1cb19aa26fa7e8
bopes <clcblee@gmail.com>
2016-06-26 14:22:02 -0700

    Add event routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
 
-
-  get 'battles/index'
-
-  get 'battles/show'
-
   resources :sides, only: [:index, :show]
   resources :players, only: [:index, :show]
   resources :armies, only: [:index, :show]
   resources :campaigns, only: [:index, :show]
   resources :battles, only: [:indx, :show]
+  resources :events, only: [:index, :show]
 
   root 'static_pages#home'
 
