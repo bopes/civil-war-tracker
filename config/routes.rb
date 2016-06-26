bfabe9f5b4d41d6ada464b46c7e7215071476750
bopes <clcblee@gmail.com>
2016-06-26 12:50:35 -0700

    Remove events routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
 
+  get 'battles/index'
+
+  get 'battles/show'
+
   resources :sides, only: [:index, :show]
   resources :players, only: [:index, :show]
   resources :armies, only: [:index, :show]
   resources :campaigns, only: [:index, :show]
+  resources :battles, only: [:indx, :show]
 
   root 'static_pages#home'
 
