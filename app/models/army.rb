3a1dc568281397ee2f9416f8d8e94e979911c81b
bopes <clcblee@gmail.com>
2016-06-26 14:19:23 -0700

    Add battles and events associations

--- app/models/army.rb
+++ app/models/army.rb
@@ -2,6 +2,7 @@ class Army < ActiveRecord::Base
 
   belongs_to :side, inverse_of: :armies
   has_many :campaigns, inverse_of: :army
-  has_and_belongs_to_many :battles, through: :campaigns
+  has_and_belongs_to_many :battles
+  has_and_belongs_to_many :events
 
 end
