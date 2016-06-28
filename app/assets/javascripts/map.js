e24f065a55acc668803d220adbb3f9bba0313d58
bopes <clcblee@gmail.com>
2016-06-27 20:54:11 -0700

    Turn method to IFEE

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,1 +1,17 @@
-map.js
+
+(function initMap() {
+  var myLatLng = {lat: <%= @latitude %>, lng: <%= @longitude %>};
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 35.6795, lng: -84.4038},
+    scrollwheel: false,
+    zoom: 5
+  });
+
+  var marker = new google.maps.Marker({
+    map: map,
+    position: myLatLng
+    // title: <%= @battleName %>
+  });
+
+})
