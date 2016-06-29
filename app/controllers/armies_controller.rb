d15aa219a59c72989678688fa1ff189b1cdf5a6f
bopes <clcblee@gmail.com>
2016-06-28 21:40:04 -0700

    Add strong params

--- app/controllers/armies_controller.rb
+++ app/controllers/armies_controller.rb
@@ -4,8 +4,29 @@ class ArmiesController < ApplicationController
     @confederate_armies = find_confederates(Army)
   end
 
+  def new
+    @army = Army.new
+  end
+
+  def create
+    @army = Army.create(army_params)
+    if @army.save
+      flash[:success] = "Army successfully created."
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
   def show
     @army = Army.find(params[:id])
     @all_events = @army.battles + @army.events
   end
+
+  private
+
+    def army_params
+      params.require(:army).permit(:name, :side_id)
+    end
+
 end
