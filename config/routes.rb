0045614f26605fe85d24e3f7750eb50193749982
bopes <clcblee@gmail.com>
2016-06-28 21:41:05 -0700

    Add new army route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :sides, only: [:index, :show]
-  resources :players, only: [:index, :show]
-  resources :armies, only: [:index, :show]
-  resources :campaigns, only: [:index, :show]
-  resources :battles, only: [:indx, :show]
-  resources :events, only: [:index, :show]
+  resources :sides, only: [:index, :show, :new, :create]
+  resources :players, only: [:index, :show, :new, :create]
+  resources :armies, only: [:index, :show, :new, :create]
+  resources :campaigns, only: [:index, :show, :new, :create]
+  resources :battles, only: [:indx, :show, :new, :create]
+  resources :events, only: [:index, :show, :new, :create]
 
   root 'static_pages#home'
 
