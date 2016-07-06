bed16a98355611245855b4bcd524cbc4b6dd6386
bopes <clcblee@gmail.com>
2016-07-06 15:41:07 -0700

    Add inverse association for army

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -2,5 +2,5 @@ class Battle < ActiveRecord::Base
   belongs_to :location
   belongs_to :victor, class_name: "Side"
   belongs_to :campaign
-  has_and_belongs_to_many :armies
+  has_and_belongs_to_many :armies, inverse_of: :battles
 end
