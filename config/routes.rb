892f60098b0ad0991c635be6369d3ee94f9cd192
bopes <clcblee@gmail.com>
2016-07-06 15:42:20 -0700

    Add all routes for armies

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :sides, only: [:index, :show, :new, :create]
   resources :players, only: [:index, :show, :new, :create]
-  resources :armies, only: [:index, :show, :new, :create]
+  resources :armies
   resources :campaigns, only: [:index, :show, :new, :create]
   resources :battles, only: [:indx, :show, :new, :create]
   resources :events, only: [:index, :show, :new, :create]
