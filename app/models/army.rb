ce652278a8cb9d2cb0d7149b0636f2dc0097e1e8
bopes <clcblee@gmail.com>
2016-07-06 15:40:52 -0700

    Add inverse associations for battles, events

--- app/models/army.rb
+++ app/models/army.rb
@@ -2,8 +2,8 @@ class Army < ActiveRecord::Base
 
   belongs_to :side, inverse_of: :armies
   has_many :campaigns, inverse_of: :army
-  has_and_belongs_to_many :battles
-  has_and_belongs_to_many :events
+  has_and_belongs_to_many :battles, inverse_of: :armies
+  has_and_belongs_to_many :events, inverse_of: :armies
 
   validates :side_id, presence: true
   validates :name, presence: true
