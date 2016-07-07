a605907d4ac28f3cd1fe43f2ef29046bb1939914
bopes <clcblee@gmail.com>
2016-07-07 16:04:46 -0700

    Add events, battles, campaigns, all_events virtual attributes

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -3,4 +3,21 @@ class Rank < ActiveRecord::Base
   belongs_to :player, inverse_of: :ranks
   belongs_to :army
 
+  def events
+    army.events.select { |event| (self.begin_date..self.end_date).cover?(event.begin_date) }
+  end
+
+  def battles
+    army.battles.select { |battle| (self.begin_date..self.end_date).cover?(battle.begin_date) }
+  end
+
+  def campaigns
+    army.campaigns.select { |campaign| (self.begin_date..self.end_date).cover?(campaign.begin_date) }
+  end
+
+  def all_events
+    events = self.events + self.campaigns + self.battles.reject { |battle| battle.campaign }
+    events.sort_by { |event| event.begin_date }
+  end
+
 end
