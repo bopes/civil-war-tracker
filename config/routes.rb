cc2788fdc46b751636c80c415b1ac9d9bee09d5f
bopes <clcblee@gmail.com>
2016-07-07 15:09:32 -0700

    Change battles#index to engagments url

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'engagements' => 'battles#index'
+
   resources :sides, only: [:index, :show, :new, :create]
   resources :players, only: [:index, :show, :new, :create]
   resources :armies
   resources :campaigns, only: [:index, :show, :new, :create]
-  resources :battles, only: [:index, :show, :new, :create]
+  resources :battles, only: [:show, :new, :create]
   resources :events, only: [:index, :show, :new, :create]
   resources :locations
 
