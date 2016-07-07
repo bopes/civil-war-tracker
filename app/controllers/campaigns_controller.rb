34095b545aac953d58757d260fedca902de9a9c3
bopes <clcblee@gmail.com>
2016-07-07 14:25:05 -0700

    Pass @commanders to show

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -12,5 +12,9 @@ class CampaignsController < ApplicationController
     @all_events = @campaign.battles + @campaign.events
     @all_events.sort_by! { |event| event.begin_date }
     @locations = @all_events.map{ |event| {lat: event.location.lat, lng: event.location.long } }
+    # @commanders = Rank.where("army = ? AND begin_date < ? AND end_date > ?", @campaign.army, @campaign.begin_date, @campaign.end_date)
+    @commanders = Rank.where(army: @campaign.army)
+    @commanders.select { |rank| rank.begin_date < @campaign.begin_date && rank.end_date > @campaign.end_date }
+
   end
 end
