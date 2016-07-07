a9bb57beeedb5ad161a477425c1429c0ba3c4985
bopes <clcblee@gmail.com>
2016-07-06 18:07:33 -0700

    Add locations routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :campaigns, only: [:index, :show, :new, :create]
   resources :battles, only: [:indx, :show, :new, :create]
   resources :events, only: [:index, :show, :new, :create]
+  resources :locations
 
   root 'static_pages#home'
 
