798cf5c6ff99c796672795ecb9b276d8fb329201
bopes <clcblee@gmail.com>
2016-07-07 16:04:18 -0700

    Add events, battles, campaigns, all_events virtual attributes

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,28 @@ class Player < ActiveRecord::Base
   belongs_to :side, inverse_of: :players
   has_many :ranks, inverse_of: :player
 
+  def battles
+    battles = []
+    ranks.each { |rank| battles += rank.battles }
+    battles
+  end
+
+  def event
+    events = []
+    ranks.each { |rank| events += rank.events }
+    events
+  end
+
+  def campaigns
+    campaigns = []
+    ranks.each { |rank| campaigns += rank.campaigns }
+    campaigns
+  end
+
+  def all_events
+    events = []
+    ranks.each { |rank| events += rank.all_events }
+    events
+  end
+
 end
