c62eda22168cfb6467875b6b844790e786a0760f
bopes <clcblee@gmail.com>
2016-07-07 15:05:45 -0700

    organize #show

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -9,12 +9,11 @@ class CampaignsController < ApplicationController
 
   def show
     @campaign = Campaign.find(params[:id])
-    @all_events = @campaign.battles + @campaign.events
-    @all_events.sort_by! { |event| event.begin_date }
-    @locations = @all_events.map{ |event| {lat: event.location.lat, lng: event.location.long } }
-    # @commanders = Rank.where("army = ? AND begin_date < ? AND end_date > ?", @campaign.army, @campaign.begin_date, @campaign.end_date)
-    @commanders = Rank.where(army: @campaign.army)
-    @commanders.select { |rank| rank.begin_date < @campaign.begin_date && rank.end_date > @campaign.end_date }
 
+    @all_events = (@campaign.battles + @campaign.events).sort_by! { |event| event.begin_date }
+
+    @locations = @all_events.map{ |event| {lat: event.location.lat, lng: event.location.long } }
+
+    @commanders = Rank.where(army: @campaign.army).select { |rank| rank.begin_date < @campaign.begin_date && rank.end_date > @campaign.end_date }
   end
 end
