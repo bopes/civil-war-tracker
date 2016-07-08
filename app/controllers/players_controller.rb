7830c2876e1881c1b17f24282d4161077e35ce3a
bopes <clcblee@gmail.com>
2016-07-07 21:52:35 -0700

    Add destroy route

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -13,4 +13,35 @@ class PlayersController < ApplicationController
     @player = Player.new
   end
 
+  def create
+    @player = Player.create(player_params)
+    if @player.save
+      # @player.battles += Battle.where("id IN (?)", params[:army][:battles])
+      # @army.events += Event.where("id IN (?)", params[:army][:events])
+      flash[:success] = "#{@player.name} added successfully!"
+      redirect_to @player
+    else
+      flash.now[:danger] = "Unable to save. Please try again."
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @player = Player.find(params[:id])
+    @player.delete
+    redirect_to players_path
+  end
+
+  private
+
+    def player_params
+      params.require(:player).permit(:name, :side_id, :bio)
+    end
+
 end
