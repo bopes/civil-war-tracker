279f6542a45dff3d5ee2155b23d65012736e2b04
bopes <clcblee@gmail.com>
2016-07-07 22:25:39 -0700

    Remove default test

--- test/models/army_test.rb
+++ test/models/army_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ArmyTest < ActiveSupport::TestCase
 
   def setup
-    @army = Army.new(name: "Army of the Tennessee", side_id: 1)
+    @army = armies(:union_army)
   end
 
   test 'should be valid' do
@@ -19,10 +19,5 @@ class ArmyTest < ActiveSupport::TestCase
     @army.name = '         '
     assert_not @army.valid?
   end
-
-  test 'do nothing' do
-    assert_not false
-  end
-
 
 end
