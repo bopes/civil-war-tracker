d24c054d9e6df4cd84afa364699954968737cc5d
bopes <clcblee@gmail.com>
2016-07-07 21:53:42 -0700

    Add full resources for player

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'engagements' => 'battles#index'
 
   resources :sides, only: [:index, :show, :new, :create]
-  resources :players, only: [:index, :show, :new, :create]
+  resources :players
   resources :armies
   resources :campaigns, only: [:index, :show, :new, :create]
   resources :battles, only: [:show, :new, :create]
