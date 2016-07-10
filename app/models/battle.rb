5301783f89e566b635830f6611c1e7a9c4a5fd88
bopes <clcblee@gmail.com>
2016-07-09 17:38:52 -0700

    Add validations

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,6 +1,11 @@
 class Battle < ActiveRecord::Base
+
   belongs_to :location
   belongs_to :victor, class_name: "Side"
   belongs_to :campaign
   has_and_belongs_to_many :armies, inverse_of: :battles
+
+  validates :location_id, presence: true
+  validates :victor_id, presence: true
+
 end
