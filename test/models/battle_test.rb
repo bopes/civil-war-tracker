6ab992fa92848c73b6885ebd9d1bb3e54daa76f2
bopes <clcblee@gmail.com>
2016-07-09 17:40:40 -0700

    Add battle model tests

--- test/models/battle_test.rb
+++ test/models/battle_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class BattleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @battle = battles(:test_battle)
+  end
+
+  test 'should be valid' do
+    assert @battle.valid?
+  end
+
+  test 'location should be present' do
+    @battle.location = nil
+    assert_not @battle.valid?
+  end
+
+  test 'name should be present' do
+    @battle.victor = nil
+    assert_not @battle.valid?
+  end
+
 end
