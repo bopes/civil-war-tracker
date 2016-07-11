759b2c53ac9234ca7cc295db3bb6cd02d4cf23ee
bopes <clcblee@gmail.com>
2016-07-11 16:40:27 -0700

    Add geocoding for new locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,7 +9,13 @@ class LocationsController < ApplicationController
 
   def create
     @location = Location.create(location_params)
-
+    if @location.save
+      flash[:success] = "Location added successfully!"
+      redirect_to @location
+    else
+      flash.now[:danger] = "Unable to find location. Please try again."
+      render 'new'
+    end
   end
 
   def show
@@ -31,7 +37,14 @@ class LocationsController < ApplicationController
   private
 
     def location_params
-      params.require(:location).permit(:city, :state, :county)
+      city = params[:location][:city]
+      state = params[:location][:state]
+      loc_raw = Geokit::Geocoders::GoogleGeocoder.geocode "#{city}, #{state}"
+      return {  city: city,
+                state: loc_raw.state_name,
+                county: loc_raw.district,
+                lat: loc_raw.lat,
+                long: loc_raw.lng }
     end
 
 end
