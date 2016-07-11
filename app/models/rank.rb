6c52f1c62f8956d55eda54d15df587ae73ee4f4b
bopes <clcblee@gmail.com>
2016-07-11 16:43:04 -0700

    Bug fix for battles, events when rank has no end date

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -5,7 +5,11 @@ class Rank < ActiveRecord::Base
   belongs_to :army
 
   def events
-    army.events.select { |event| (self.begin_date..self.end_date).cover?(event.begin_date) }
+    if self.end_date
+      army.events.select { |event| (self.begin_date..self.end_date).cover?(event.begin_date) }
+    else
+      army.event.select { |event| self.begin_date < event.begin_date }
+    end
   end
 
   def battles
