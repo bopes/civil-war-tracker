9f4a9a377e8f0a19e96a12117a32828cccd916b2
bopes <clcblee@gmail.com>
2016-07-10 19:03:40 -0700

    Add restful routes for locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,36 @@
 class LocationsController < ApplicationController
+  def index
+    @locations = Location.order(:state).order(:city)
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def location_params
+
+    end
+
 end
