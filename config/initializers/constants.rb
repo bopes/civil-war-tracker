0f8f9aa7cfb39fbf90264c716ccfe8da137bf86d
bopes <clcblee@gmail.com>
2016-07-10 20:35:51 -0700

    Add constants file with PLURALS

---
+++ config/initializers/constants.rb
@@ -0,0 +1,8 @@
+PLURALS = { Army: "armies",
+            Battle: "battles",
+            Campaign: "campaigns",
+            Event: "events",
+            Location: "locations",
+            Player: "players",
+            Rank: "rank",
+            Side: "sides" }
