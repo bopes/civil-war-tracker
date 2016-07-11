bf14635d2848d4d021584abdd1b2551d877be0b6
bopes <clcblee@gmail.com>
2016-07-11 15:50:49 -0700

    Add new, create, edit, update methods

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -11,4 +11,49 @@ class BattlesController < ApplicationController
                         lng: @battle.location.long}
     @locations = [battle_location]
   end
+
+  def new
+    @sides = Side.all
+    @campaigns = Campaign.all
+    @locations = Location.all
+    @battle = Battle.new
+  end
+
+  def create
+    @battle = Battle.create(battle_params)
+    if @battle.save
+      @battle.armies += Army.where("id IN (?)", params[:battle][:armies])
+      flash[:success] = "#{@battle.name} added successfully!"
+      redirect_to @battle
+    else
+      flash.now["Unable to save. Please try again."]
+      render 'new'
+    end
+  end
+
+  def edit
+    @sides = Side.all
+    @campaigns = Campaign.all
+    @locations = Location.all
+    @battle = Battle.find(params[:id])
+  end
+
+  def update
+    @battle = Battle.find(params[:id])
+    if @battle.update(battle_params)
+      @battle.armies.delete_all
+      @battle.armies += Army.where("id IN (?)", params[:battle][:armies])
+      flash[:success] = "#{@battle.name} updated successfully!"
+      redirect_to @battle
+    else
+      flash.now["Unable to save. Please try again."]
+      render 'edit'
+    end
+  end
+
+  private
+
+    def battle_params
+      params.require(:battle).permit(:begin_date, :end_date, :location_id, :victor_id, :campaign_id, :conclusion, :description, :name)
+    end
 end
