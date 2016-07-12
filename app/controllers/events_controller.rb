3cee98f989eb8ea02444056ea25dcfc198f623fd
bopes <clcblee@gmail.com>
2016-07-12 11:19:11 -0700

    Add destroy method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,13 +13,42 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.create(event_params)
+    if @event.save
+      flash[:success] = "#{@event.name} added successfully!"
+      redirect_to @event
+    else
+      flash.now[:danger] = "Unable to save. Please try again."
+      render 'new'
+    end
   end
 
   def edit
-
+    @campaigns = Campaign.all
+    @locations = Location.all
+    @event = Event.find(params[:id])
   end
 
   def update
-
+    @event = Event.find(params[:id])
+    if @event.update_attributes(event_params)
+      flash[:success] = "#{@event.name} updated successfully!"
+      redirect_to @event
+    else
+      flash.now[:danger] = "Unable to save. Please try again."
+      render 'edit'
+    end
   end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to 'engagements'
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:name, :description, :location_id, :begin_date, :end_date, :campaign_id)
+    end
 end
