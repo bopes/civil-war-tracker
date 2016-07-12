0727baf97bd052911b741ca2d87538347ffeaf00
bopes <clcblee@gmail.com>
2016-07-11 19:53:22 -0700

    Add delete method

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -21,11 +21,21 @@ class RanksController < ApplicationController
   end
 
   def update
-
+    @rank = Rank.find(params[:id])
+    if @rank.update_attributes(rank_params)
+      flash[:success] = "Rank successfully updated for #{@rank.player.name}!"
+      redirect_to @rank.player
+    else
+      flash.now[:danger] = "Unable to save. Please try again."
+      render 'new'
+    end
   end
 
   def destroy
-
+    @rank = Rank.find(params[:id])
+    @player = @rank.player
+    @rank.destroy
+    redirect_to @player
   end
 
   private
