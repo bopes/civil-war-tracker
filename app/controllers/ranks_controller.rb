26bf051dec5f3367399be4fc8676ad076bff7309
bopes <clcblee@gmail.com>
2016-07-11 19:45:09 -0700

    Add create

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,12 +1,22 @@
 class RanksController < ApplicationController
   def new
+    @sides = Side.all
+    @rank = Rank.new(player_id: params[:player_id])
   end
 
   def create
+    @rank = Rank.create(rank_params)
+    if @rank.save
+      flash[:success] = "New rank successfully added for #{@rank.player.name}!"
+      redirect_to @rank.player
+    else
+      flash.now[:danger] = "Unable to save. Please try again."
+      render 'new'
+    end
   end
 
   def edit
-
+    @rank = Rank.find(params[:id])
   end
 
   def update
@@ -20,6 +30,6 @@ class RanksController < ApplicationController
   private
 
     def rank_params
-
+      params.require(:rank).permit(:player_id, :army_id, :title, :begin_date, :end_date)
     end
 end
