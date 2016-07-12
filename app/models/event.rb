147ddc8f631bacbd3f25113da1458f2161d3e999
bopes <clcblee@gmail.com>
2016-07-12 15:49:48 -0700

    Add location coordinates

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,9 @@ class Event < ActiveRecord::Base
   belongs_to :campaign
   belongs_to :location
   has_and_belongs_to_many :armies, inverse_of: :battles
+
+  def location_coordinates
+    [self.location.coordinates]
+  end
+
 end
