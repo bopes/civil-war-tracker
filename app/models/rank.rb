0437967403b3ceac3a15c7b9f9dea05d26823b4d
bopes <clcblee@gmail.com>
2016-07-11 17:29:37 -0700

    Fix typo bug

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -8,7 +8,7 @@ class Rank < ActiveRecord::Base
     if self.end_date
       army.events.select { |event| (self.begin_date..self.end_date).cover?(event.begin_date) }
     else
-      army.event.select { |event| self.begin_date < event.begin_date }
+      army.events.select { |event| self.begin_date < event.begin_date }
     end
   end
 
