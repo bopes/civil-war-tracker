d6894950ba3a614103f8b70217040b156eba9ee1
bopes <clcblee@gmail.com>
2016-07-11 19:18:11 -0700

    Add Rank views, methods

--- app/controllers/ranks_controller.rb
+++ app/controllers/ranks_controller.rb
@@ -1,2 +1,25 @@
 class RanksController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def rank_params
+
+    end
 end
