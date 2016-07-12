3be531255d6045e0e744ee6b5c8ce445e5f65379
bopes <clcblee@gmail.com>
2016-07-12 14:38:59 -0700

    Add location name

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -23,21 +23,29 @@ class LocationsController < ApplicationController
   end
 
   def edit
-
+    @location = Location.find(params[:id])
   end
 
   def update
-
+    @location = Location.find(params[:id])
+    if @location.update_attributes(location_params)
+      flash[:success] = "Location added successfully!"
+      redirect_to @location
+    else
+      flash.now[:danger] = "Unable to find location. Please try again."
+      render 'new'
+    end
   end
 
   def destroy
-
+    @location = Location.find(params[:id])
+    @location.delete
+    redirect_to 'index'
   end
 
   private
 
     def location_params
-      p params
       city = params[:location][:city]
       county = params[:location][:county]
       state = params[:location][:state]
@@ -48,7 +56,8 @@ class LocationsController < ApplicationController
                 state: loc_raw.state_name,
                 county: loc_raw.district,
                 lat: loc_raw.lat,
-                long: loc_raw.lng }
+                long: loc_raw.lng,
+                location_name: params[:location][:location_name] }
     end
 
 end
