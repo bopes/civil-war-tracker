599f20fd216c1c8a74aa0d78c23cbee2ce4685bf
bopes <clcblee@gmail.com>
2016-07-12 17:28:42 -0700

    Add engagements method

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,10 @@ class StaticPagesController < ApplicationController
   def home
     @sides = Side.all
   end
+
+  def engagements
+    @sides = Side.all
+    @battles = Battle.where(campaign: nil)
+    @events = Event.where(campaign: nil)
+  end
 end
