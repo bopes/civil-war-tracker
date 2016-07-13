7e8c507add13ef9095098eee1569d2391b80724b
bopes <clcblee@gmail.com>
2016-07-12 18:46:09 -0700

    Add battles, events to homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
     @sides = Side.all
+    @battles = Battle.where(campaign: nil)
+    @events = Event.where(campaign: nil)
   end
 
   def engagements
