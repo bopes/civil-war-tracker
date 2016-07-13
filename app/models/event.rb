149b68224f8f96dd1724d037bbd045e5c06f62a8
bopes <clcblee@gmail.com>
2016-07-13 14:40:41 -0700

    Add begin_date validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,7 @@ class Event < ActiveRecord::Base
   has_and_belongs_to_many :armies, inverse_of: :battles
 
   validates :begin_date, presence: true
+  validates :location_id, presence: true
 
   def location_coordinates
     [self.location.coordinates]
