49a26a06776ecfbadd9972bd4b6f1868f5170855
bopes <clcblee@gmail.com>
2016-07-29 16:48:38 -0700

    Comment out methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def find_union(search_class)
-    union = Side.find_by(name: "The Union")
-    search_class.where(side: union)
-  end
+  # def find_union(search_class)
+  #   union = Side.find_by(name: "The Union")
+  #   search_class.where(side: union)
+  # end
 
-  def find_confederates(search_class)
-    confederacy = Side.find_by(name: "The Confederacy")
-    search_class.where(side: confederacy)
-  end
+  # def find_confederates(search_class)
+  #   confederacy = Side.find_by(name: "The Confederacy")
+  #   search_class.where(side: confederacy)
+  # end
 
 end
